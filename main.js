819a648ed1932b3cb908b55876dafc73e61ca413
Mrs-Archer <i.juskevic@gmail.com>
2020-12-04 22:37:06 +0000

    Copleted Game

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const gameBoard = [
 ];
 
 let playerOnesTurn = true;
+let turnCounter = 0;
 
 gameBoard.forEach(function (e, iy) {
   e.forEach(function (e, ix) {
@@ -21,9 +22,72 @@ gameBoard.forEach(function (e, iy) {
           currentBoardPosition.addClass("la-player-2");
         }
       }
-      console.log(gameBoard);
+      turnCounter++;
+      addTheMessage();
     });
   });
 });
 
-function checkForWin() {}
+function checkForWin() {
+  if (
+    gameBoard[0][0] === gameBoard[0][1] &&
+    gameBoard[0][2] === gameBoard[0][1]
+  ) {
+    if (gameBoard[0][0]) return gameBoard[0][0];
+  }
+  if (
+    gameBoard[1][0] === gameBoard[1][1] &&
+    gameBoard[1][2] === gameBoard[1][1]
+  ) {
+    if (gameBoard[1][0]) return gameBoard[1][0];
+  }
+  if (
+    gameBoard[2][0] === gameBoard[2][1] &&
+    gameBoard[2][2] === gameBoard[2][1]
+  ) {
+    if (gameBoard[2][0]) return gameBoard[2][0];
+  }
+  if (
+    gameBoard[0][0] === gameBoard[1][0] &&
+    gameBoard[2][0] === gameBoard[1][0]
+  ) {
+    if (gameBoard[2][0]) return gameBoard[2][0];
+  }
+  if (
+    gameBoard[0][1] === gameBoard[1][1] &&
+    gameBoard[2][1] === gameBoard[1][1]
+  ) {
+    if (gameBoard[2][1]) return gameBoard[2][1];
+  }
+  if (
+    gameBoard[0][2] === gameBoard[1][2] &&
+    gameBoard[2][2] === gameBoard[1][2]
+  ) {
+    if (gameBoard[2][2]) return gameBoard[2][2];
+  }
+  if (
+    gameBoard[0][0] === gameBoard[1][1] &&
+    gameBoard[2][2] === gameBoard[1][1]
+  ) {
+    if (gameBoard[0][0]) return gameBoard[0][0];
+  }
+  if (
+    gameBoard[0][2] === gameBoard[1][1] &&
+    gameBoard[2][0] === gameBoard[1][1]
+  ) {
+    if (gameBoard[1][1]) return gameBoard[1][1];
+  }
+}
+
+function addTheMessage() {
+  if (turnCounter === 9) {
+    $(".winning-message").html("Game end in a Draw");
+  }
+  if (checkForWin()) {
+    $(".winning-message").html(
+      "Congratulations Player - " + checkForWin() + " !!!"
+    );
+  }
+
+  console.log(checkForWin());
+}
